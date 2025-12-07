f52fc157d0b9ab015e64060e2be906d5fd876e87
Quang Tuấn <91524625+TuanLe30122003@users.noreply.github.com>
2025-12-08 02:21:36 +0700

    feat(UI): update mobile UI of wwebsite

--- app/my-orders/layout.jsx
+++ app/my-orders/layout.jsx
@@ -6,7 +6,7 @@ const MyOrdersLayout = ({ children }) => {
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-center">
       <Navbar />
-      <main className="flex-1 py-8 w-full px-32 flex flex-col gap-4">
+      <main className="flex-1 py-8 w-full lg:px-32 md:px-16 px-4 flex flex-col gap-4">
         {children}
       </main>
       <Footer />
--- app/search/layout.jsx
+++ app/search/layout.jsx
@@ -6,7 +6,7 @@ const WishlistLayout = ({ children }) => {
   return (
     <div className="w-full min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 py-8 w-full px-32 flex flex-col gap-4">
+      <main className="flex-1 py-8 w-full lg:px-32 md:px-16 px-4 flex flex-col gap-4">
         {children}
       </main>
       <Footer />
